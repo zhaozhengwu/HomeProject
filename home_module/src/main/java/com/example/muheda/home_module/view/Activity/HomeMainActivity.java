8c67e72075fbf5d3ec68bf04ad29cc7d25d4190b
muheda <yunwei@muheda.com>
2018-09-06 17:38:03 +0800

    de

--- home_module/src/main/java/com/example/muheda/home_module/view/Activity/HomeMainActivity.java
+++ home_module/src/main/java/com/example/muheda/home_module/view/Activity/HomeMainActivity.java
@@ -2,23 +2,21 @@ package com.example.muheda.home_module.view.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
-import com.example.muheda.base_module.Utils.IntentToActivity;
 import com.example.muheda.home_module.R;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeMainActivity extends AppCompatActivity {
     private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_main);
        /* tv=findViewById(R.id.tv);
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                IntentToActivity.skipToActivity(MainActivity.this,Main2Activity.class);
+                IntentToActivity.skipToActivity(HomeMainActivity.this,Main2Activity.class);
             }
         });*/
     }
