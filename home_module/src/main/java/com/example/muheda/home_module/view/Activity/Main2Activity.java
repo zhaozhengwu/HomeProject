d396a2f217adc3b80e2fe1a16a1ff6f6a53f397d
muheda <yunwei@muheda.com>
2018-09-06 17:00:34 +0800

    Initial commit

---
+++ home_module/src/main/java/com/example/muheda/home_module/view/Activity/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.muheda.home_module.view.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.muheda.home_module.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ home_module/src/main/java/com/example/muheda/home_module/view/Activity/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.muheda.home_module.view.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.muheda.base_module.Utils.IntentToActivity;
+import com.example.muheda.home_module.R;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentToActivity.skipToActivity(MainActivity.this,Main2Activity.class);
+            }
+        });
+    }
+}
