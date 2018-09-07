816da044487af39e0284c5be7174c23453094c8d
muheda <yunwei@muheda.com>
2018-09-07 18:43:41 +0800

    1.0od

--- app/src/main/java/com/example/muheda/homeproject/MainActivity.java
+++ app/src/main/java/com/example/muheda/homeproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.muheda.homeproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.muheda.home_module.view.Activity.HomeMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this, HomeMainActivity.class));
     }
 }
